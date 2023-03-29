1de973c92238413862440c678b814e13cbe0060a
SebastianKurrle <kurrlesebasitan@gmail.com>
2023-03-29 23:52:40 +0200

    authenticate and filter

--- tigerfrontend/src/components/Authenticate.vue
+++ tigerfrontend/src/components/Authenticate.vue
@@ -1,3 +1,28 @@
 <script setup>
+    import { ref } from 'vue'
+    import { useAuthenticateStore } from '../stores/authenticate';
 
+    // stores
+    const authenticateStore = useAuthenticateStore()
+
+    const password = ref('')
+
+    const login = () => {
+        authenticateStore.authentciate(password.value)
+        password.value = ''
+    }
 </script>
+
+<template>
+    <div>
+        <h4 class="text-center">Authentifizieren um Bilder und Videos sehen zu können</h4>
+
+        <form @submit.prevent="login">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" id="password" class="form-control" v-model="password">
+        </form>
+    </div>
+</template>
+
+<style scoped>
+</style>
--- tigerfrontend/src/views/FilterResultView.vue
+++ tigerfrontend/src/views/FilterResultView.vue
@@ -1,3 +1,24 @@
 <script setup>
+    import { useFilterStore } from '../stores/filter';
+    import Image from '../components/Image.vue';
+    import Video from '../components/Video.vue';
 
+    // stores
+    const filterStore = useFilterStore()
 </script>
+
+<template>
+    <div>
+        <h4 class="text-center">Filter Ergebnisse</h4>
+
+        <div class="flex justify-center">
+            <div class="row mb-3 gap-3">
+              <Image v-for="img in filterStore.filterRes.img" :key="img.id" :image="img" class="col-md-4"/>
+              <Video v-for="video in filterStore.filterRes.video" :key="video.id" :video="video"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
